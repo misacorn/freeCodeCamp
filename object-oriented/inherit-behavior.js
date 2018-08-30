d6bd2277588fa64b3a0dfb7be75c1dcf5df841e0
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2018-08-30 22:44:52 +0300

    children's object inherits the parent's prototype

--- object-oriented/inherit-behavior.js
+++ object-oriented/inherit-behavior.js
@@ -14,3 +14,20 @@ duck.eat(); // Should print "nom nom nom"
 beagle.eat(); // Should print "nom nom nom" 
 
 //Object.create(obj) creates a new object, and sets obj as the new object's prototype. Recall that the prototype is like the "recipe" for creating an object. By setting the prototype of duck anf beagle to be Animal's prototype, you are effectively giving the duck and beagle instances the same "recipe" as any other instance of Animal.
+
+function Animal() { }
+
+Animal.prototype = {
+  constructor: Animal,
+  eat: function() {
+    console.log("nom nom nom");
+  }
+};
+
+function Dog() { }
+
+// Add your code below this line
+Dog.prototype = Object.create(Animal.prototype)
+
+let beagle = new Dog();
+beagle.eat();  // Should print "nom nom nom"
