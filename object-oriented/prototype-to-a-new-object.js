d2ad610c937fe637c6be3b20141c316d5ad4ac56
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2018-08-30 20:37:15 +0300

    constructor property

--- object-oriented/prototype-to-a-new-object.js
+++ object-oriented/prototype-to-a-new-object.js
@@ -19,3 +19,19 @@ Birds.prototype = {
     console.log("My name is " + this.name);
   }
 }
+
+function Dog(name) {
+  this.name = name; 
+}
+
+// Modify the code below this line
+Dog.prototype = {
+  
+  numLegs: 2, 
+  eat: function() {
+    console.log("nom nom nom"); 
+  }, 
+  describe: function() {
+    console.log("My name is " + this.name); 
+  }
+};
