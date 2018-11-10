9e7befa7765858a40f4d3ddf0ae020a6543aa845
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2018-11-10 22:14:07 +0200

    use an IIFE to create a module

---
+++ object-oriented/module.js
@@ -0,0 +1,24 @@
+let isCuteMixin = function(obj) {
+  obj.isCute = function() {
+    return true;
+  };
+};
+let singMixin = function(obj) {
+  obj.sing = function() {
+    console.log("Singing to an awesome tune");
+  };
+};
+
+let funModule = (() => {
+  return {
+    isCuteMixin: obj => {
+  obj.isCute = () => {
+    return true;
+    };
+  },
+  singMixin: obj => {
+  obj.sing = () => {
+    console.log("Singing to an awesome tune");
+  };
+}
+}}) ();
