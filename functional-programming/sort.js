c93aaf2514f44c1cacc55e7165f5865bd15fcd73
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2018-11-11 18:36:12 +0200

    sort()

---
+++ functional-programming/sort.js
@@ -0,0 +1,15 @@
+function ascendingOrder(arr) {
+  return arr.sort(function(a, b) {
+    return a - b;
+  });
+}
+ascendingOrder([1, 5, 2, 3, 4]);
+// Returns [1, 2, 3, 4, 5]
+
+function reverseAlpha(arr) {
+  return arr.sort(function(a, b) {
+    return a < b;
+  });
+}
+reverseAlpha(['l', 'h', 'z', 'b', 's']);
+// Returns ['z', 's', 'l', 'h', 'b']
