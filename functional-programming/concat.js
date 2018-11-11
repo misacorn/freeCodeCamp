487771fa16c738f557f5387a12145676a5fe7a9c
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2018-11-11 17:11:14 +0200

    concat()

---
+++ functional-programming/concat.js
@@ -0,0 +1,15 @@
+//adding new item to an array
+function nonMutatingPush(original, newItem) {
+  return original.concat(newItem);
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingPush(first, second);
+
+//adding the second array to the first array
+function nonMutatingConcat(original, attach) {
+  return original.concat(attach);
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingConcat(first, second);
