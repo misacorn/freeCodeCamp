914c0c998d90da19a47d3dfb478e01edcabdf2f1
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2018-11-11 18:46:38 +0200

    sÅtring.prototype.split()

---
+++ functional-programming/split.js
@@ -0,0 +1,13 @@
+function splitify(str) {
+  return str.split(/\W/)
+}
+splitify("Hello World,I-am code");
+//["Hello", "World", "I", "am", "code"]
+
+var str = "Hello World";
+var bySpace = str.split(" ");
+// Sets bySpace to ["Hello", "World"]
+
+var otherString = "How9are7you2today";
+var byDigits = otherString.split(/\d/);
+// Sets byDigits to ["How", "are", "you", "today"]
