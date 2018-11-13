a26a2517f061b0e0a13d411c3dae0d7292fc3411
Huong Ngo <huong.ngo2@myy.haaga-helia.fi>
2018-11-13 14:47:45 +0200

    differences between 2 arrays

---
+++ intermediate-algorithm-scripting/diff-two-arrays.js
@@ -0,0 +1,11 @@
+function diffArray(arr1, arr2) {
+  let newArr = [];
+  let newArr1 = arr1.filter(x => !arr2.includes(x));
+  let newArr2 = arr2.filter(x => !arr1.includes(x));
+  if (newArr2.length !== 0 || newArr1.length !== 0) {
+    newArr = newArr1.concat(newArr2);
+  }
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
